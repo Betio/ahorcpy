5a37be574dbf11c4ffaede5c4c79ba7892695b66
Betio <zurdoku3687@gmail.com>
2014-11-03 16:09:58 -0600

    inicio
    agregué un par de prints explicando qué es básicamente el programa.

---
+++ PRINCIPAL.py
@@ -0,0 +1,22 @@
+## ARCHIVO PRINCIPAL ##
+print 'bienvenido al examen final de IPC 2014'
+print 'el programa se trata del famoso juego, ahorcado's
+
+
+class Carro: 
+    def __init__(self, gasolina):
+        self.gasolina = gasolina
+        print "tenemos ",gasolina," litros"
+        
+    def arrancar(self):
+        if self.gasolina >0 :
+            print "Arrancando"
+        else:
+            print "No arranca"
+            
+    def conducir(self):
+        if self.gasolina > 0:
+            self.gasolina -= 1
+            print "quedan ",self.gasolina," litros"
+        else:
+            print "no se mueve"
