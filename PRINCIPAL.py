5334b5fd1b7be13651a01031bc241db0a1a5f5b0
Betio <zurdoku3687@gmail.com>
2014-11-02 21:54:47 -0600

    Create PRINCIPAL.py

---
+++ PRINCIPAL.py
@@ -0,0 +1,18 @@
+## ARCHIVO PRINCIPAL ##
+class Carro: 
+    def __init__(self, gasolina):
+        self.gasolina = gasolina
+        print "tenemos ",gasolina," litros"
+        
+    def arrancar(self):
+        if self.gasolina >0 :
+            print "Arrancando"
+        else:
+            print "No arranca"
+            
+    def conducir(self):
+        if self.gasolina > 0:
+            self.gasolina -= 1
+            print "quedan ",self.gasolina," litros"
+        else:
+            print "no se mueve"
